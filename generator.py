4dfac33e9c892b916bb64582165178501f475485
nurbolatbissenov <76535291+nurbolatbissenov@users.noreply.github.com>
2022-05-19 14:01:29 +0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -102,12 +102,3 @@ print(elapsed_time)
 def powers_of_two(n):
     return [2**x for x in range(n+1)]
 print(powers_of_two(2))
-
-
-n = int(input())
-mass = []
-for i in range(n):
-    number = int(input())
-    mass.append(number)
-for a in mass:
-    print(a)
